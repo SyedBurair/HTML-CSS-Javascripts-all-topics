5a3bd6404a2ab688197ac886239b31497f22ea51
SyedBurair <105945686+SyedBurair@users.noreply.github.com>
2025-06-20 12:33:35 +0100

    Add files via upload

---
+++ object.js
@@ -0,0 +1,9 @@
+//object lecture
+const profile ={
+    name: 'Burair Rizvi',
+    posts: 194,
+    followers: 569000,
+    following: 4,
+    bio: 'To educate someone is the highest privilege'
+}
+console.log(profile)

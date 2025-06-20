12377dcb43bfdbbcdb6d18a5cf1f1dff5fc9a2e9
SyedBurair <105945686+SyedBurair@users.noreply.github.com>
2025-06-20 12:36:54 +0100

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,54 @@
+let a= 5;
+let b= 6;
+
+ console.log("a + b = ", a+b)
+ console.log("a * b = ", a*b)
+console.log("a - b = ", a-b)
+console.log("a / b = ", a/b)
+console.log("a % b = ", a%b)//% this sign is called modulus 
+console.log("a ** b = ", a**b)// double star which means power of two 2 called exponentiation
+
+
+//uniray operators which is increment or decrement
+let c= 5;
+ let d= 6;
+ c++;// answer is 6 because in increment plus one 
+ d++;// answer is 7
+console.log("c = ", c)
+console.log("d = ", d)
+
+
+//Assignment operators such as +=, -=, %=, /=.
+//first is +=
+let e= 7;
+let f=8;
+e+=5; // e= e+5
+f += 4;// f= f+4
+console.log('e=',e)//answer is 12
+console.log('f=',f)//answer is 12
+
+
+//comperison operators such as ==(equal to), !=(not equal to), ===(equalto and type), !==(not equal to and type )
+//<=, >=, <, > 
+
+let g=6;
+let h=5;
+
+console.log("g==h", g==h)// answer false
+console.log("g!=h", g!=h)// answer true
+
+
+//Logical operators such as( ||, &&, !)
+ 
+let i = 8;
+let j = 7;
+
+let cond1= 8>7;
+let cond2= i==8;
+
+console.log("cond1&&cond2=",cond1&&cond2)
+
+let cond3= 8<7;
+let cond4= i==8;
+
+console.log("cond1||cond2=",cond1||cond2)

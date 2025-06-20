da198a5098bd40e85d37a775ca1442f6e8a23663
SyedBurair <105945686+SyedBurair@users.noreply.github.com>
2025-06-20 12:36:02 +0100

    Add files via upload

---
+++ cond_stat.js
@@ -0,0 +1,113 @@
+// // Conditional statement
+// // IF condition
+// let mde= "light";
+// let clor;
+
+// if(mode==="dark")
+// {
+//     console.log("black")
+// }
+// if(mode==="light")
+//     {
+//         console.log("white")
+//     }
+
+//     console.log(color);
+// // IF-Else condition
+
+// let mode= "light";
+// let color;
+
+// if(mode==="light"){
+
+//     console.log("white")
+// }
+// else{
+//     console.log("BLACK")
+// }
+
+// console.log(color)
+// // another example 
+// let num=34;
+
+// if(num%2 === 0)
+//     {
+//         console.log(num, "is even")
+//     }
+
+//     else
+//     {
+//         console.log(num,"is odd")
+//     }
+
+//     // else-if 
+
+//     let moode= "yellow";
+//     let colour;
+
+//     if(moode === "dark"){
+//         console.log("black")
+//     }
+//     else if(moode=== "blue")
+//         {
+//             console.log("blue")
+//         }
+
+//         else if(moode=== "gray")
+//             {
+//                 console.log("gray")
+//             }
+//             else if(moode=== "red")
+//                 {
+//                     console.log("red")
+//                 } 
+//     else{
+//         console.log("white")
+//     }    
+    
+//     console.log(color);
+
+//     // ternery operators 
+//     // in this operators is like 
+//     // condition?true output : false output
+
+//     // promt 
+
+//     let number= prompt("Enter a number:")
+
+//     if(number%5 === 0){
+
+//         console.log("number is multiply by 5")
+//     }
+
+//     console.log(number);
+
+    // prac question
+
+    let score=67;
+    let grade;
+
+    if(score>=90&&score<=100)
+        {
+            console.log("grade A")
+        }
+        else if(score>=70&&score<=89)
+            {
+                console.log("grade B")
+            }
+            else if(score>=60&&score<=69)
+                {
+                    console.log("grade C")
+                }
+                else if(score>=50&&score<=59)
+                    {
+                        console.log("grade D")
+                    }    
+    if(score>=0&&score<=49)      
+        {
+            console.log("grade F")
+        }          
+
+        console.log(grade)
+
+       
